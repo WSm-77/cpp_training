25609ee2e8839bd78c6384f4d9d740c327c43dc0
wiktor <wiktor.sedzimir@gmail.com>
2024-05-14 21:36:08 +0200

    Add templates

--- array/include/array.h
+++ array/include/array.h
@@ -1,19 +1,43 @@
 #pragma once
+#include <iostream>
 
+template <typename elemType>
 class Array
 {
 private:
 
-    int *array;
+    elemType *array;
     int arraySize;
 
 public:
 
-    Array(int size);
-    ~Array();
+    Array(int size) : arraySize(size){
+        array = new elemType[size];
+    }
 
-    int& at(int index);
-    int size();
-    void print_array();
+    ~Array(){
+        delete [] array;
+    }
+
+    elemType& at(int index){
+        return array[index];
+    }
+
+    int size(){
+        return arraySize;
+    }
+
+    void print_array(){
+        for (int i = 0; i < arraySize; i++){
+            std::cout<<array[i]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+
+    void fill(elemType val){
+        for (int i = 0; i < arraySize; i++){
+            at(i) = val;
+        }
+    }
 
 };
--- array/main/main.cpp
+++ array/main/main.cpp
@@ -7,19 +7,29 @@ int main(){
     std::cout<<"enter initial array size: ";
     std::cin>>arrayInitSize;
 
-    Array myArray(arrayInitSize);
+    Array<int> myIntArray(arrayInitSize);
+    Array<double> myDoubleArray(arrayInitSize);
+    Array<std::string> myStringArray(arrayInitSize);
 
-    for (int i = 0; i < myArray.size(); i++){
-        myArray.at(i) = 7;
-    }
+    myIntArray.fill(7);
+    myDoubleArray.fill(3.14);
+    myStringArray.fill("alice");
 
     std::cout<<"values in my array before change:\t"<<std::endl;
-    myArray.print_array();
+    myIntArray.print_array();
+    myDoubleArray.print_array();
+    myStringArray.print_array();
 
-    myArray.at(1) = 3;
+    if (1 < myIntArray.size()){
+        myIntArray.at(1) = 3;
+        myDoubleArray.at(1) = 2.71;
+        myStringArray.at(1) = "bob";
+    }
 
-    std::cout<<"values in my array after change:\t"<<std::endl;
-    myArray.print_array();
+    std::cout<<std::endl<<"values in my array after change:\t"<<std::endl;
+    myIntArray.print_array();
+    myDoubleArray.print_array();
+    myStringArray.print_array();
 
     return 0;
 }
