a3c1168c6930d0bdffea6e4fc2e703639f97f26e
wiktor <wiktor.sedzimir@gmail.com>
2024-05-14 21:52:38 +0200

    Method definitions outside class

--- array/include/array.h
+++ array/include/array.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <iostream>
 
 template <typename elemType>
 class Array
@@ -11,33 +10,47 @@ private:
 
 public:
 
-    Array(int size) : arraySize(size){
-        array = new elemType[size];
-    }
+    Array(int size);
+    ~Array();
 
-    ~Array(){
-        delete [] array;
-    }
-
-    elemType& at(int index){
-        return array[index];
-    }
-
-    int size(){
-        return arraySize;
-    }
-
-    void print_array(){
-        for (int i = 0; i < arraySize; i++){
-            std::cout<<array[i]<<" ";
-        }
-        std::cout<<std::endl;
-    }
-
-    void fill(elemType val){
-        for (int i = 0; i < arraySize; i++){
-            at(i) = val;
-        }
-    }
+    elemType& at(int index);
+    int size();
+    void print_array();
+    void fill(elemType val);
 
 };
+
+template <typename elemType>
+Array<elemType>::Array(int size) : arraySize(size) {
+    array = new elemType[size];
+}
+
+template <typename elemType>
+Array<elemType>::~Array() {
+    delete [] array;
+}
+
+template <typename elemType>
+elemType& Array<elemType>::at(int index){
+    return array[index];
+}
+
+template <typename elemType>
+int Array<elemType>::size(){
+    return arraySize;
+}
+
+template <typename elemType>
+void Array<elemType>::print_array(){
+    for (int i = 0; i < arraySize; i++){
+        std::cout<<array[i]<<" ";
+    }
+    std::cout<<std::endl;
+}
+
+template <typename elemType>
+void Array<elemType>::fill(elemType val){
+    for (int i = 0; i < arraySize; i++){
+        array[i] = val;
+    }
+}
